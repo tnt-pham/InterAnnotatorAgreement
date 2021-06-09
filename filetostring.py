b1f674f9c31b0cda717b1450fe6fa1f0e268873d
thommy24 <pham.thomas@web.de>
2021-06-10 01:38:49 +0200

    Finish filetostring

--- filetostring.py
+++ filetostring.py
@@ -1,30 +1,38 @@
 # -*- coding: utf-8 -*-
 
 # Thomas N. T. Pham (nhpham@uni-potsdam.de)
-# 03-Jun-2021
+# 09-Jun-2021
 # Python 3.7
 # Windows 10
-"""Reads text from a file."""
-# logger name? same as iaa?
+"""Supplementary class for file reading."""
 
-class FileToString:  # or rather FileToString?
+
+class FileToString:
+    """Retrieves text from a file.
+
+    Args:
+        file (str): Path of the file.
+        encoding (str): File encoding. Defaults to 'utf-8'.
+
+    Attributes:
+        text (str): Text read from the file.
+
+    Raises:
+        FileNotFoundError: If given file path does not exist.
+        PermissionError: If given file path leads to a directory.
+    """
     def __init__(self, file, encoding="utf-8"):
-        self.text = self.file_to_string(file, encoding=encoding)
+        self.text = self._file_to_string(file, encoding=encoding)
 
-    def file_to_string(self, file, encoding="utf-8"):
+    def _file_to_string(self, file, encoding="utf-8"):
+        """Retrieves text from a file."""
         try:
             with open(file, 'r', encoding=encoding) as read_f:
                 text = read_f.read()
         except FileNotFoundError as fnf:
-            fnf_msg = file +" does not exist."
+            fnf_msg = file + " does not exist."
             raise FileNotFoundError(fnf_msg).with_traceback(fnf.__traceback__)
         except PermissionError as pe:
             pe_msg = file + " does not lead to a file."
-            raise FileNotFoundError(pe_msg).with_traceback(pe.__traceback__)
+            raise PermissionError(pe_msg).with_traceback(pe.__traceback__)
         return text
-
-
-if __name__ == "__main__":
-    fr = FileToString("testdata\\text1.txt")
-    print(fr.text)
-    print(len(fr.text.split()))
